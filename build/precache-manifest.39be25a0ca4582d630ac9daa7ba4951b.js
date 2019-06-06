bdaf88066ef5d37e331e408702653dcc4c6c4876
GRTSolutionsLTD <rivki@grth.co.il>
2019-06-06 20:05:05 +0300

    upload project

---
+++ build/precache-manifest.39be25a0ca4582d630ac9daa7ba4951b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9942630ee5e7832c9b28d3928e362c88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f7a98bed23cac33f849",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "160f502e5ab8f1613c50",
+    "url": "/static/css/main.087278fa.chunk.css"
+  },
+  {
+    "revision": "6f7a98bed23cac33f849",
+    "url": "/static/js/2.0bad1663.chunk.js"
+  },
+  {
+    "revision": "160f502e5ab8f1613c50",
+    "url": "/static/js/main.b3e274bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
